5e17d0a51fd6cb4476e947b1adb82b7cd62228df
Tomas Ambrogi <tomi.ambro94@gmail.com>
2025-05-09 21:32:30 -0300

    added bluetooth protocol

--- MeshNode.swift
+++ MeshNode.swift
@@ -1,6 +1,8 @@
 import Foundation
 import MultipeerConnectivity
 import Network
+import CoreBluetooth
+
 
 // MARK: - Transport Protocol
 
@@ -9,6 +11,120 @@ protocol ChatTransport {
     func start()
     func sendMessage(_ text: String)
 }
+
+
+class BluetoothManager: NSObject, ChatTransport {
+    private var peripheralManager: CBPeripheralManager!
+    private var centralManager: CBCentralManager!
+    private var chatCharacteristic: CBMutableCharacteristic!
+    private var subscribedCentrals: [CBCentral] = []
+    private var discoveredPeripherals: [CBPeripheral] = []
+    private var chatCharacteristics: [CBPeripheral: CBCharacteristic] = [:]
+
+    private let serviceUUID = CBUUID(string: "C0DE1000-FEED-FEED-FEED-C0DEC0FFEE01")
+    private let chatCharacteristicUUID = CBUUID(string: "C0DE1001-FEED-FEED-FEED-C0DEC0FFEE01")
+
+    var onMessageReceived: ((String, String) -> Void)?
+
+    override init() {
+        super.init()
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func start() {
+        // Both central and peripheral will begin once state updates to poweredOn
+    }
+
+    func sendMessage(_ text: String) {
+        guard let data = text.data(using: .utf8) else { return }
+
+        // Send as peripheral
+        peripheralManager.updateValue(data, for: chatCharacteristic, onSubscribedCentrals: nil)
+
+        // Send as central
+        for (peripheral, characteristic) in chatCharacteristics {
+            peripheral.writeValue(data, for: characteristic, type: .withResponse)
+        }
+    }
+}
+
+// MARK: - Peripheral Role
+
+extension BluetoothManager: CBPeripheralManagerDelegate {
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        guard peripheral.state == .poweredOn else { return }
+
+        chatCharacteristic = CBMutableCharacteristic(
+            type: chatCharacteristicUUID,
+            properties: [.notify],
+            value: nil,
+            permissions: [.readable]
+        )
+
+        let service = CBMutableService(type: serviceUUID, primary: true)
+        service.characteristics = [chatCharacteristic]
+
+        peripheralManager.add(service)
+        peripheralManager.startAdvertising([
+            CBAdvertisementDataServiceUUIDsKey: [serviceUUID],
+            CBAdvertisementDataLocalNameKey: "BluetoothChat"
+        ])
+    }
+
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        subscribedCentrals.append(central)
+    }
+}
+
+// MARK: - Central Role
+
+extension BluetoothManager: CBCentralManagerDelegate, CBPeripheralDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        guard central.state == .poweredOn else { return }
+        centralManager.scanForPeripherals(withServices: [serviceUUID])
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !discoveredPeripherals.contains(peripheral) {
+            discoveredPeripherals.append(peripheral)
+            peripheral.delegate = self
+            centralManager.connect(peripheral, options: nil)
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.discoverServices([serviceUUID])
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            peripheral.discoverCharacteristics([chatCharacteristicUUID], for: service)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral,
+                    didDiscoverCharacteristicsFor service: CBService,
+                    error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+        for characteristic in characteristics where characteristic.uuid == chatCharacteristicUUID {
+            chatCharacteristics[peripheral] = characteristic
+            peripheral.setNotifyValue(true, for: characteristic)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral,
+                    didUpdateValueFor characteristic: CBCharacteristic,
+                    error: Error?) {
+        if let data = characteristic.value,
+           let text = String(data: data, encoding: .utf8) {
+            onMessageReceived?(peripheral.name ?? "BluetoothPeer", text)
+        }
+    }
+}
+
 
 // MARK: - Multipeer Manager (Apple Devices)
 
@@ -109,7 +225,7 @@ class WiFiDirectManager: ChatTransport {
 
     func sendMessage(_ text: String) {
         guard let connection = connection else {
-            print("[WifiDirect] No connection available.")
+            // print("[WifiDirect] No connection available.")
             return
         }
         let data = text.data(using: .utf8)
@@ -137,8 +253,8 @@ class CrossPlatformTransportManager: ChatTransport {
     init() {
         let wifiTransport = WiFiDirectManager()
         let multiTransport = MultipeerManager()
-        transports.append(wifiTransport)
-        transports.append(multiTransport)
+        let bluetoothTransport = BluetoothManager()
+        transports = [wifiTransport, multiTransport, bluetoothTransport]
     }
 
     func start() {
